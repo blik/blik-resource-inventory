93ebc335dad94b3b4effa26b23a9a923de338420
Aleksey Bogoslovskyi <Aleksey_Bogoslovsky@priocom.com>
2012-09-13 17:32:41 +0300

    Add feature(with UT) to obtain BlikRI configuration params. Add list of supported entity types

--- blik/inventory/core/config.py
+++ blik/inventory/core/config.py
@@ -1,43 +1,49 @@
-
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Purpose: Configure Blik Inventory
+Created: 13.09.2012
+Author:  Konstantin Andrusenko & Aleksey Bogoslovskyi
+"""
 
 import yaml
-
+import sys
+sys.path.append('../../../')
 from inv_exceptions import BIException
-
+from blik.inventory.backend.mongo import *
 
 #configuration parameters names
 CP_BACKEND = 'backend'
 CP_DB_CONNSTRING = 'db_connect_string'
+CP_DB_NAME = 'db_name'
 
 class InventoryConfiguration:
     def __init__(self, **kw_args):
         self.backend = kw_args.get(CP_BACKEND, None)
         self.db_connect_string = kw_args.get(CP_DB_CONNSTRING, None)
+        self.db_name = kw_args.get(CP_DB_NAME, None)
 
-    def __get_config_param(self, config_obj, param_name):
+    def _get_config_param(self, config_obj, param_name):
         value = config_obj.get(param_name, None)
         if value is None:
             raise BIException('Configuration parameter <%s> is not found!'% param_name)
-
         return value
 
     def from_dict(self, config_obj):
-        self.backend = self.__get_config_param(config_obj, CP_BACKEND)
-        self.db_connect_string = self.__get_config_param(config_obj, CP_DB_CONNSTRING)
+        self.backend = self._get_config_param(config_obj, CP_BACKEND)
+        self.db_connect_string = self._get_config_param(config_obj, CP_DB_CONNSTRING)
+        self.db_name = self._get_config_param(config_obj, CP_DB_NAME)
 
-    def from_config_file(self, config_file):
-        if not os.path.exists(config_file):
-            raise BIException('File %s is not found!'% config_file)
-
-        f_obj = open(file_path)
-
+    def get_backend_db(self, config_file):
         try:
-            conf_obj = yaml.load(f_obj)
-        except Exception, err:
-            raise BIException('File %s is not found!'%file_path)
-        finally:
-            f_obj.close()
-
-        self.from_dict(conf_obj)
-
+            conf_obj = yaml.load(open(config_file))
+            self.from_dict(conf_obj)
+            if self.backend == "mongodb":
+                backend_conn_obj = MongoDatabaseAPI(self.db_connect_string, self.db_name)
+            else:
+                raise BIException("Blik Resource Inventory don't support <%s> backend type!" %self.backend)
+            return backend_conn_obj
+        except IOError, err:
+            raise Exception("Configuration file is invalid:", err.strerror)
+        except yaml.YAMLError, exc:
+            raise Exception("Error in configuration file:", exc)
